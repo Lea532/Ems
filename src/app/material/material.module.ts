deefd7a383c5551f174edd8425b811dd3d55d529
Tobias Küfe <t.kuefe@neusta.de>
2024-12-18 11:43:20 +0100

    Initial Commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,18 @@
+import { ModuleWithProviders, NgModule, Type} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+
+const matModules = [
+  CommonModule,
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    matModules
+  ],
+  exports: [
+    matModules,
+  ]
+})
+export class MaterialModule { }
